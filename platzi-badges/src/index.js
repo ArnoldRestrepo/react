2c316520338940495bef08813be3d1504d6ebea7
Arnold Restrepo <restrepo_arnold@outlook.com>
2019-05-09 15:04:29 -0500

    React Router v4

--- platzi-badges/src/index.js
+++ platzi-badges/src/index.js
@@ -9,11 +9,9 @@ import React from "react"; // Analogía de Create Element
 import ReactDOM from "react-dom"; // Analogía de Append Child
 import "bootstrap/dist/css/bootstrap.css";
 import "./global.css";
-
-import BadgeNew from "./pages/BadgeNew.js";
-import Badges from "./pages/Badges.js";
+import App from "./components/App.js";
 
 const container = document.getElementById("app");
 
 // ReactDOM.render(__qué__, __dónde__);
-ReactDOM.render(<Badges />, container);
+ReactDOM.render(<App />, container);
